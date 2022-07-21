09d9c9fd798f48718124c35548d28394f583b7ff
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-07-21 23:51:51 +0530

    Created Sexercise component and ExerciseCard Component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,28 @@
 import React, {useEffect,useState}from 'react'
-import { Pagination } from '@mui/material'
+import { Pagination } from '@mui/material/Pagination'
 import { exerciseOptions,fetchData } from '../utils/fetchData'
 import { Box, Stack,Typography } from '@mui/material'
-const Exercises = () => {
+import ExerciseCard from './ExerciseCard'
+const Exercises = ({exercises, setExercises,bodyPart}) => {
   return (
-    <div>Exercises</div>
+    <Box id ="exercises" 
+    sx={{
+      mt:{lg: '110px'}
+    }}
+    mt="50px"
+    p="20px">
+<Typography variant="h3" mb="46px">
+  Showing Results
+</Typography>
+<Stack direction="row" sx={{
+  gap:{lg:'110px',xs:'50px'}
+}}
+flexWrap="wrap" justifyContent="center">
+{exercises.map((exercise,index)=>(
+ <ExerciseCard key={index} exercise={exercise} />
+))}
+</Stack>
+    </Box>
   )
 }
 
