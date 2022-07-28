bb9cd87735f6ab0b52a22393101ee0dbffb2acf2
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-07-28 23:59:57 +0530

    added

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -7,22 +7,10 @@ import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
-    const [exerciseDetail, setExerciseDetail] = useState({})
-    const{ id } = useParams()
-    useEffect(() => {
-        const fetchExerciseData = async () => {
-          const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
-
-      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
-      setExerciseDetail(exerciseDetailData);
-    }
-
-    fetchExerciseData()
-  }, [id])
+    
   return (
     <Box >
-    <Detail />
+    <Detail  />
     <ExerciseVideos/>
     <SimilarExercises/>
     </Box>
